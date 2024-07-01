035483124e98f533c2b56d55fcde3ed99fb6d0dc
next <example>
2024-07-01 17:07:45 +0800

    为首页添加导航栏(

--- nextapp/src/app/page.tsx
+++ nextapp/src/app/page.tsx
@@ -1,12 +1,35 @@
 import Image from "next/image";
+import React, { memo } from "react";
+import internal from "stream";
+import styles from "./component.module.css"
+
+
+const TopBar:React.FC<{props:{itemlist:Array<any>}}> = function(props){
+  return (
+    <>
+      <div className={styles.bar}>
+        <div className={styles.logo}>
+            一个标题
+        </div>
+        <div className={styles.option}>
+
+        </div>
+        <div className={styles.info}>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+const Topbar = memo(TopBar)
 
 export default function Home() {
+  const data = {itemlist:[]}
   return (
     <div>
       <div className="external">
-        {
-          
-        }
+        <Topbar props={data}></Topbar>
       </div>
     </div>
   );
